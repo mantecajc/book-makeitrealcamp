ce2713fe27277e8dda9b2d1f7eed7c678c78f5ca
mantecajc <65590523+javierc435@users.noreply.github.com>
2020-08-21 10:50:59 +0200

    Add url_image

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_21_084115) do
+ActiveRecord::Schema.define(version: 2020_08_21_084643) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_08_21_084115) do
     t.decimal "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image_url"
   end
 
 end
